7d5663c2f57343be1ef204f8c5b772fe6a80f44a
ARAFAT <munshiarafathussain@gmail.com>
2023-12-15 02:00:05 +0600

    Tested

--- folderMGNT.cpp
+++ folderMGNT.cpp
@@ -15,6 +15,9 @@ struct translation{
 
 void compressedAllFiles( vector<string> filePaths);
 void decompressedAllFiles( vector<string> filePaths);
+void huffCompression(string inpPath, string folderPath);
+void huffDecompression(string inpPath, string folderPath);
+
 void write_from_uChar(unsigned char,unsigned char*,int,FILE*);
 void huffcom(string filePath, string compressFolderPath);
 
@@ -36,6 +39,359 @@ struct ersel{
 bool erselcompare0(ersel a,ersel b){
     return a.number<b.number;
 }
+
+
+
+
+class FileAccessor
+{
+public:
+	static std::shared_ptr<std::vector<char>> GetSymbVectPtr(const std::string& path);
+
+	static void WriteSymbVectToFile(const std::shared_ptr<std::vector<char>>& inpVect, const std::string& path);
+
+	static long int GetFileSize(const std::string& path);
+};
+
+std::shared_ptr<std::vector<char>> FileAccessor::GetSymbVectPtr(const std::string& path)
+{
+	std::shared_ptr<std::vector<char>> symbVectorPtr = std::shared_ptr<std::vector<char>>(new std::vector<char>);
+	std::ifstream file(path, std::ios::binary);
+	if (file.is_open() && !file.fail())
+	{
+		char t;
+		while (!file.eof()) {
+			file.read((char*)&t, sizeof(char));
+			symbVectorPtr->push_back(t);
+		}
+	}
+	else
+	{
+		return nullptr;
+	}
+	file.close();
+	symbVectorPtr->pop_back();
+	return symbVectorPtr;
+};
+
+void FileAccessor::WriteSymbVectToFile(const std::shared_ptr<std::vector<char>>& inpVect, const std::string& path) {
+	std::ofstream file;
+	file.open(path, std::ios::binary);
+	if (file.is_open() && !file.fail())
+	{
+		unsigned int from = 0;
+		char* buff = new char[UCHAR_MAX];
+		while (from < inpVect->size())
+		{
+			if (from + UCHAR_MAX < inpVect->size())
+			{
+				for (int i = 0; i < UCHAR_MAX; ++i)
+				{
+					buff[i] = inpVect->at(from + i);
+				}
+				file.write(buff, sizeof(char)*UCHAR_MAX);
+				from += UCHAR_MAX;
+			}
+			else {
+				for (int i = 0; i < inpVect->size() - from; ++i)
+				{
+					buff[i] = inpVect->at(from + i);
+				}
+				file.write(buff, sizeof(char)*(inpVect->size() - from));
+				from = inpVect->size();
+			}
+		}
+	}
+	else {
+		//error
+	}
+	file.close();
+}
+
+long FileAccessor::GetFileSize(const std::string & path)
+{
+	std::ifstream file(path, std::ios::binary);
+	file.seekg(0, std::ios::end);
+	long int file_size = file.tellg();
+	return file_size;
+}
+
+
+
+
+class HuffLeaf {
+public:
+	char value;
+	HuffLeaf* left;
+	HuffLeaf* right;
+	HuffLeaf* parent;
+	bool isLeaf;
+	unsigned long int freq;
+public:
+	HuffLeaf(char value_t, HuffLeaf* parent_t, int freq_t);
+
+	HuffLeaf(HuffLeaf* right_t, HuffLeaf* left_t);
+
+	~HuffLeaf();
+
+	std::string GetCode(char inp);
+};
+
+class Huffman {
+private:
+	static std::map<char, unsigned int> GetFreqVect(std::shared_ptr<std::vector<char>> inpVect);
+
+	static char pack_byte(bool bits[8]);
+
+	static HuffLeaf* CreateTree(std::map<char, unsigned int> freqVect);
+
+public:
+
+	static void encode(const std::shared_ptr<std::vector<char>>& inpVect, const std::string& path);
+
+	static std::shared_ptr<std::vector<char>> decode(const std::string& path);
+};
+
+
+
+
+
+
+HuffLeaf::HuffLeaf(char value_t, HuffLeaf* parent_t, int freq_t)
+{
+	value = value_t;
+	parent = parent_t;
+	freq = freq_t;
+	isLeaf = true;
+	right = nullptr;
+	left = nullptr;
+}
+
+HuffLeaf::HuffLeaf(HuffLeaf* right_t, HuffLeaf* left_t) {
+	right = right_t;
+	left = left_t;
+	freq = right_t->freq + left_t->freq;
+	value = '\0';
+	isLeaf = false;
+	parent = nullptr;
+}
+
+HuffLeaf::~HuffLeaf()
+{
+	delete left;
+	delete right;
+}
+
+std::string HuffLeaf::GetCode(char inp)
+{
+	if (left->isLeaf)
+	{
+		if (left->value == inp)
+			return "1";
+	}
+	else
+	{
+		std::string v = left->GetCode(inp);
+		if (v[0] != 'n')
+			return "1" + v;
+	}
+
+	if (right->isLeaf)
+	{
+		if (right->value == inp)
+			return "0";
+	}
+	else
+	{
+		std::string v = right->GetCode(inp);
+		if (v[0] != 'n')
+			return "0" + v;
+	}
+	return "n";
+}
+
+std::map<char, unsigned int> Huffman::GetFreqVect(std::shared_ptr<std::vector<char>> inpVect)
+{
+	std::map<char, unsigned int> res;
+	for (auto c = inpVect->begin(); c < inpVect->end(); ++c)
+	{
+		auto iter = res.find(*c);
+		if (iter == res.end())
+		{
+			res[*c] = 1;
+		}
+		else
+		{
+			if (res[*c] < INT32_MAX) res[*c]++;
+		}
+	}
+	return res;
+}
+
+HuffLeaf* Huffman::CreateTree(std::map<char, unsigned int> freqVect)
+{
+	std::vector<HuffLeaf*> huffVect;
+	for (auto f = freqVect.begin(); f != freqVect.end(); ++f)
+	{
+		huffVect.push_back(new HuffLeaf(f->first, nullptr, f->second));
+	}
+
+	while (huffVect.size() > 1)
+	{
+		std::sort(huffVect.begin(), huffVect.end(), [](const HuffLeaf* lhs, const HuffLeaf* rhs) {
+			return lhs->freq > rhs->freq; });
+		HuffLeaf* rightLeaf = huffVect.back();
+		huffVect.pop_back();
+		HuffLeaf* leftLeaf = huffVect.back();
+		huffVect.pop_back();
+		HuffLeaf* fuzed = new HuffLeaf(rightLeaf, leftLeaf);
+		rightLeaf->parent = fuzed;
+		leftLeaf->parent = fuzed;
+		huffVect.push_back(fuzed);
+	}
+
+	return huffVect.at(0);
+}
+
+char Huffman::pack_byte(bool bits[8]) {
+	char res(0);
+	for (unsigned i(8); i--;)
+	{
+		res <<= 1;
+		res |= char(bits[i]);
+	}
+	return res;
+}
+
+void Huffman::encode(const std::shared_ptr<std::vector<char>>& inpVect, const std::string& path) {
+	std::ofstream out;
+	out.open(path, std::ios::binary);
+	if (out.is_open()) {
+
+		unsigned int symbCount = inpVect->size();
+		out.write((char*)&symbCount, sizeof(unsigned int));
+
+		std::map<char, unsigned int> freqVect = GetFreqVect(inpVect);
+		int freqSize = freqVect.size();
+
+		out.write((char*)&freqSize, sizeof(int));
+		for (auto f = freqVect.begin(); f != freqVect.end(); ++f)
+		{
+			out.write((char*)&f->first, sizeof(char));
+			out.write((char*)&f->second, sizeof(int));
+		}
+
+		std::shared_ptr<HuffLeaf> TreeRoot = std::shared_ptr<HuffLeaf>(CreateTree(freqVect));
+		std::map<char, std::string> encMap;
+
+		std::vector<bool> codedBools;
+		std::string codeS;
+		for (int i = 0; i < inpVect->size(); ++i)
+		{
+			if (!encMap.count(inpVect->at(i)))
+				encMap[inpVect->at(i)] = TreeRoot->GetCode(inpVect->at(i));
+			codeS = encMap[inpVect->at(i)];
+			for (int j = 0; j < codeS.size(); ++j)
+				codedBools.push_back(codeS[j] == '1');
+			if (codedBools.size() >= 8)
+			{
+				char s(0);
+				bool bits[8];
+				for (int i = 0; i < 8; ++i) {
+					bits[i] = codedBools.front();
+					codedBools.erase(codedBools.begin());
+				}
+				s = pack_byte(bits);
+				out.write((char*)&s, sizeof(char));
+			}
+		}
+		if (codedBools.size() > 0)
+		{
+			while (codedBools.size() < 8)
+				codedBools.push_back(false);
+			char s(0);
+			bool bits[8];
+			for (int i = 0; i < 8; ++i) {
+				bits[i] = codedBools.front();
+				codedBools.erase(codedBools.begin());
+			}
+			s = pack_byte(bits);
+			out.write((char*)&s, sizeof(char));
+		}
+		out.close();
+	}
+	else
+	{
+		//error
+		return;
+	}
+}
+
+std::shared_ptr<std::vector<char> > Huffman::decode(const std::string & path) {
+	std::ifstream fin;
+	fin.open(path, std::ios::binary);
+	if (fin.is_open())
+	{
+		unsigned int symbCount;
+		fin.read((char*)&symbCount, sizeof(unsigned int));
+		int freqVectSize;
+		std::map<char, unsigned int> freqVect;
+		fin.read((char*)&freqVectSize, sizeof(int));
+		unsigned int freq;
+		char val;
+		for (unsigned int i = 0; i < freqVectSize; ++i)
+		{
+			fin.read((char*)&val, sizeof(char));
+			fin.read((char*)&freq, sizeof(int));
+			freqVect[val] = freq;
+		}
+
+		std::shared_ptr<HuffLeaf> TreeRoot = std::shared_ptr<HuffLeaf>(CreateTree(freqVect));
+		HuffLeaf* currLeaf = TreeRoot.get();
+
+		std::shared_ptr<std::vector<char>> outVect = std::shared_ptr<std::vector<char>>(new std::vector<char>);
+		std::deque<bool> boolsToDecode;
+
+		while (!fin.eof())
+		{
+			char s;
+			fin.read((char*)&s, sizeof(char));
+
+			for (int i = 0; i < 8; ++i)
+				boolsToDecode.push_back(s & (1 << i));
+
+			while (boolsToDecode.size() > 0)
+			{
+				if (currLeaf->isLeaf) {
+					outVect->push_back(currLeaf->value);
+					currLeaf = TreeRoot.get();
+				}
+				if (boolsToDecode.front())
+					currLeaf = currLeaf->left;
+				else
+					currLeaf = currLeaf->right;
+				boolsToDecode.pop_front();
+			}
+			if (currLeaf->isLeaf) {
+				outVect->push_back(currLeaf->value);
+				currLeaf = TreeRoot.get();
+			}
+		}
+
+		while (outVect->size() > symbCount)
+			outVect->pop_back();
+
+		fin.close();
+		return outVect;
+	}
+	else
+	{
+		fin.close();
+		//error
+	}
+}
+
+
 
 
 
@@ -145,9 +501,9 @@ Llist *HashTable::SearchTable(const char *buff, int p)
 }
 
 // Forward declaraction of main functions
-void Compress( const string &fn, const string &outputfolder);
+void lz77Compress( const string &fn, const string &outputfolder);
 int find_match(HashTable *hash, const char *buff, int &winstart, int &current, const int size);
-void Decompress( const std::string &filename, const std::string &outputfolder);
+void lz77Decompress( const std::string &filename, const std::string &outputfolder);
 
 class BitBuffer
 {
@@ -566,6 +922,66 @@ vector<string> FolderMGMT::getFilePathList()
 }
 
 
+void compressFolderUsingLZ77( vector<string> filePaths)
+{
+    size_t commonDirPos = filePaths[0].find_last_of('/');
+    string commonDirectory = filePaths[0].substr(0, commonDirPos);
+    string decompressFolderPath = commonDirectory +  "/lzComFolder";
+
+
+    // Read the files using the file paths
+
+
+    if (mkdir(decompressFolderPath.c_str(), 0777) == 0)
+    {
+        cout << "\nCreated folder: " << decompressFolderPath << endl;
+
+        for (const string& filePath : filePaths)
+        {
+            lz77Compress(filePath, decompressFolderPath);      
+        }
+    }
+    else
+    {
+        cout << "Failed to create folder: " << decompressFolderPath << endl;
+    }
+}
+
+
+
+void compressFolderUsingHuffman( vector<string> filePaths)
+{
+    size_t commonDirPos = filePaths[0].find_last_of('/');
+    string commonDirectory = filePaths[0].substr(0, commonDirPos);
+    string decompressFolderPath = commonDirectory +  "/huffComFolder";
+
+
+    // Read the files using the file paths
+
+
+    if (mkdir(decompressFolderPath.c_str(), 0777) == 0)
+    {
+        cout << "\nCreated folder: " << decompressFolderPath << endl;
+
+        for (const string& filePath : filePaths)
+        {
+            //huffcom(filePath, decompressFolderPath);
+            //lz77Compress(filePath, decompressFolderPath);
+            huffCompression(filePath, decompressFolderPath);
+        }
+    }
+    else
+    {
+        cout << "Failed to create folder: " << decompressFolderPath << endl;
+    }
+}
+
+
+
+
+
+
+
 void compressedAllFiles( vector<string> filePaths)
 {
     size_t commonDirPos = filePaths[0].find_last_of('/');
@@ -583,7 +999,8 @@ void compressedAllFiles( vector<string> filePaths)
         for (const string& filePath : filePaths)
         {
             //huffcom(filePath, decompressFolderPath);
-            Compress(filePath, decompressFolderPath);
+            //lz77Compress(filePath, decompressFolderPath);
+            //huffCompression(filePath, decompressFolderPath);
         }
     }
     else
@@ -610,7 +1027,24 @@ void decompressedAllFiles( vector<string> filePaths)
         for (const string& filePath : filePaths)
         {
             //huffdecom(filePath, decompressFolderPath);
-            Decompress(filePath, decompressFolderPath);
+            //lz77Decompress(filePath, decompressFolderPath);
+            //huffDecompression(filePath, decompressFolderPath);
+
+            size_t lastDot = filePath.find_last_of('.');
+            string compressMode = filePath.substr(lastDot+1);
+
+            if(compressMode == "huff")
+            {
+                huffDecompression(filePath, decompressFolderPath);
+            }else if(compressMode == "lzss")
+            {
+                lz77Decompress(filePath, decompressFolderPath);
+            }else if(compressMode == "bwt")
+            {
+                cout<<"Loading..."<<endl;
+            }else{
+                cout<<"Couldn't find compress mode."<<endl;
+            }
         }
     }
     else
@@ -638,8 +1072,11 @@ int main() {
              << "1 - change path" << endl
              << "2 - output folders in path" << endl
              << "3 - output files in path" << endl
-             << "4 - Compressed all the files" << endl
-             << "5 - Decompressed all the files" << endl
+             << "4 - Compress Your Folder" << endl
+             << "5 - Compress Folder using Huffman" << endl
+             << "6 - Compress Folder using LZ77" << endl
+             << "7 - Compress Folder using BWT" << endl
+             << "8 - Decompress Your Folder" << endl
              << "9 - exit" << endl;
 
         cout << "Enter: ";
@@ -669,6 +1106,20 @@ int main() {
                 cin.get();
                 break;
             case 5:
+                cout << "\n---------------------------------------------------"
+                     << "\nCompression of [" << fmgmt->dirMGMT->getDirPath() << "]\n"
+                     << "---------------------------------------------------" << endl;
+                compressFolderUsingHuffman(fmgmt->getFilePathList());
+                cin.get();
+                break;
+            case 6:
+                cout << "\n---------------------------------------------------"
+                     << "\nCompression of [" << fmgmt->dirMGMT->getDirPath() << "]\n"
+                     << "---------------------------------------------------" << endl;
+                compressFolderUsingLZ77(fmgmt->getFilePathList());
+                cin.get();
+                break;
+            case 7:
                 {string folderName;
                 cout<<"Enter folder name: ";
                 cin>>folderName;
@@ -695,6 +1146,38 @@ int main() {
     delete fmgmt;
 
     return 0;
+}
+
+
+
+
+
+void huffCompression(string inpPath, string folderPath)
+{
+    cout<<inpPath<<endl;
+    cout<<folderPath<<endl;
+    std::shared_ptr<std::vector<char>> inpData = FileAccessor::GetSymbVectPtr(inpPath);
+	if (!inpData) return;
+
+    size_t lastSlash = inpPath.find_last_of('/');
+    string rawName = inpPath.substr(lastSlash+1);
+    string outPath = folderPath + "/" + rawName + ".huff";
+
+    Huffman::encode(inpData, outPath);
+
+}
+void huffDecompression(string inpPath, string folderPath)
+{
+    std::shared_ptr<std::vector<char>> outData = Huffman::decode(inpPath);
+
+    size_t lastSlash = inpPath.find_last_of('/');
+    string rawName = inpPath.substr(lastSlash+1);
+    size_t lastDot = rawName.find_last_of('.');
+    string name = rawName.substr(0, lastDot);
+    string outPath = folderPath + "/New-h-" + name;
+
+    FileAccessor::WriteSymbVectToFile(outData, outPath);
+
 }
 
 
@@ -1291,7 +1774,7 @@ int find_match(HashTable *hash, const char *buff, int &winstart, int &current, c
     }
 }
 
-void Compress( const string &fn, const string &outputfolder)
+void lz77Compress( const string &fn, const string &outputfolder)
 {
     int size;
         unique_ptr<char> data;
@@ -1350,7 +1833,7 @@ void Compress( const string &fn, const string &outputfolder)
 }
 
 
-void Decompress( const std::string &filename, const std::string &outputfolder)
+void lz77Decompress( const std::string &filename, const std::string &outputfolder)
 {
     int size;
         unique_ptr<char> data;
